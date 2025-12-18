205f579bf65dd298a45d92029a0fa7929dfd4659
6xigua <xingpeiyi@yeah.net>
2025-12-18 11:20:49 +0800

    feat：完善企业管理的编辑和更多功能

--- prototypes/layout.js
+++ prototypes/layout.js
@@ -12,7 +12,7 @@ function renderLayout(activePageTitle) {
     </div>
     
     <nav class="flex-1 overflow-y-auto p-4 space-y-1">
-      ${renderNavItem('enterprise.html', '企业管理', 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4', activePageTitle === '企业管理')}
+      ${renderNavItem('index.html', '企业管理', 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4', activePageTitle === '企业管理')}
       ${renderNavItem('project.html', '项目管理', 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2', activePageTitle === '项目管理')}
       ${renderNavItem('user.html', '用户管理', 'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z', activePageTitle === '用户管理')}
       ${renderNavItem('role.html', '角色管理', 'M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z', activePageTitle === '角色管理')}
