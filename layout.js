5c74de4fdf34b7a2ab5110a52405bb0b9f14e4e4
xpy <xingpeiyi@yeah.net>
2026-01-16 14:15:20 +0800

    xpydd
    update

--- layout.js
+++ layout.js
@@ -23,7 +23,7 @@ function renderLayout(activePageTitle) {
         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
         </svg>
-        <span class="text-lg font-bold">安之眸企业管理系统</span>
+        <span class="text-lg font-bold">安之眸企业管理平台</span>
       </div>
     </div>
     
